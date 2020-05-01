c9f81e522ab35c178e54932e555fd62cf9a439e3
Linh Nguyen <linh.ng754@gmail.com>
2020-05-01 16:01:21 -0400

    Initial deployment  boilerplate

--- app.py
+++ app.py
@@ -115,6 +115,7 @@ def generate_table(data, max_rows=5):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.config.suppress_callback_exceptions = True
 
