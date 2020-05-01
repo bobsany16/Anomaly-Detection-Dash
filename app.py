8c56ca9e4dfd9472b402311b73d5db7ebd3e9e6d
Linh Nguyen <linh.ng754@gmail.com>
2020-05-01 16:23:09 -0400

    Changed map files

--- app.py
+++ app.py
@@ -116,9 +116,12 @@ def generate_table(data, max_rows=5):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+
+
 
 app.config.suppress_callback_exceptions = True
+
+server = app.server
 
 
 app.layout = html.Div(children=[
