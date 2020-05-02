63a9393ff210df836a7214a6342891e835bfc9e4
Linh Nguyen <46255649+bobsany16@users.noreply.github.com>
2020-05-01 22:54:42 -0400

    Added Tabs (#5)
    * Tabs
    
    * Delete extras
    
    * Changed

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 from sklearn.utils import shuffle
 import numpy as np
+from sklearn.ensemble import IsolationForest
+from sklearn import preprocessing
 
 
 
@@ -44,8 +46,10 @@ df.drop(indexNames, inplace=True)
 df2 = shuffle(df, random_state=42)
 
 
+
 ###Change NAN grade to F###
 df2 = df2.replace(np.nan, 'F', regex=True)
+df6 = df2 #Without Lat Long for ML purposes.
 
 ###Adding Lat Long to exsiting Dataset###
 def addLatLong(type):
@@ -75,11 +79,6 @@ fig3 = px.strip(df4, x="startdate", y="grade", orientation="h", color='state')
 fig4 = px.box(df_clinton, x="state", y="adjpoll_clinton", notched=True)
 fig5 = px.box(df_trump, x="state", y="adjpoll_trump", notched=True)
 
-#fig6 = px.scatter_mapbox(df_clinton, color="state", size="adjpoll_clinton",
-                  #color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
-#fig6.show()
-
-
 fig6 = px.scatter_mapbox(df_clinton, lat="lat", lon="long", hover_name="state", hover_data=["state", "adjpoll_clinton"],
                         color='grade', size='adjpoll_clinton' , zoom=3, height=400)
 fig6.update_layout(mapbox_style="open-street-map")
@@ -90,7 +89,44 @@ fig7 = px.scatter_mapbox(df_trump, lat="lat", lon="long", hover_name="state", ho
 fig7.update_layout(mapbox_style="open-street-map")
 fig7.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-fig8 = px.scatter_matrix(df4)
+
+###Machine Learning Part###
+###########################
+###########################
+features = df6.columns
+
+#Encoding Labels for datasets#
+le1 = preprocessing.LabelEncoder()
+le2 = preprocessing.LabelEncoder()
+
+df6['state'] = le2.fit_transform(list(df6['state']))
+df6['grade'] = le1.fit_transform(list(df6['grade']))
+
+
+# Training Set
+X_train = df6[features][:4992]
+
+# Validation Set
+X_valid = df6[features][4993:len(df6)]
+
+#Fitting the Isolation Forest#
+clf = IsolationForest(n_estimators=100, max_samples='auto', behaviour ='new', contamination=0.1, random_state=42)
+clf.fit(X_train)
+
+train_scores = clf.decision_function(X_train)
+val_scores = clf.decision_function(X_valid)
+
+#Predictions & scores for Validation Set#
+X_valid['anomaly'] = clf.predict(X_valid)
+X_valid['scores'] = val_scores
+
+#Prediction & scores for Training Set#
+X_train['anomaly']=clf.predict(X_train)
+X_train['scores']=train_scores
+
+#Get State and Grade Back#
+#Add Lat and Long#
+
 
 ###Showing Original Table###
 def generate_table(data, max_rows=5):
@@ -105,27 +141,50 @@ def generate_table(data, max_rows=5):
         ])
     ])
 
+
+###Actual Dash session###
+#----------------------------------------------------------------------#
+#----------------------------------------------------------------------#
+#----------------------------------------------------------------------#
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-
-
 app.config.suppress_callback_exceptions = True
 
 
-
-
+###Main Page with Tabs###
+#----------------------------------------------------------------------#
+#----------------------------------------------------------------------#
+#----------------------------------------------------------------------#
 app.layout = html.Div(children=[
     html.H1(children='Anomaly Detection', style={
             'font-weight': '900', 'text-align': 'center'}),
     html.Div(children='''
-        A Machine Learning research project on possible anomalies detection in the 2016 US Presidential Election datasets
-    '''),
+        A Machine Learning research project on possible anomalies detection in the 2016 US Presidential Election datasets.
 
+        Project by Jason Green and Linh Nguyen
+    '''),
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Data Visualization', value='tab-1'),
+        dcc.Tab(label='Data Analysis/ML', value='tab-2'),]),
+    #Div for Tab-content to display#
+    html.Div(id='tabs-content')
+    ], style={'width': '100%', 'height': '100%', 'display': 'flex', 'justify-content': 'center', 'text-align': 'center', 'flex-direction': 'column'})
+
+@app.callback(Output('tabs-content', 'children'),
+            [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return tab1_content
+
+###Tab1_content when clicked Tab1###
+#----------------------------------------------------------------------#
+#----------------------------------------------------------------------#
+#----------------------------------------------------------------------#
+tab1_content = html.Div([
     html.Div(id='graph-display1', children=generate_table(df2),
              style={'overflow-y': 'scroll', 'display': 'flex', 'text-align': 'center', 'justify-content': 'center'}),
-
     html.H2(children='Data Visualization', style={
             'font-weight': '900', 'text-align': 'center'}),
     html.Div(children='''
@@ -148,16 +207,7 @@ app.layout = html.Div(children=[
             value='Clinton', 
             style={'margin-top': '2em','align-items': 'center', 'width': '450px', 'cursor': 'default'})]),
     ], style={'display': 'flex', 'flex-direction':'column', 'justify-content': 'center', 'align-items': 'center'}),
-
-
-    html.Div(id='graph-display', 
-
-                 style={'display': 'flex', 'flex-direction': 'row', 'flex-wrap':'wrap'}),
-
-
-
-], style={'width': '100%', 'height': '100%', 'display': 'flex', 'justify-content': 'center', 'text-align': 'center', 'flex-direction': 'column'})
-
+    html.Div(id='graph-display', style={'display': 'flex', 'flex-direction': 'row', 'flex-wrap':'wrap'})])
 
 @app.callback(
     Output(component_id='graph-display', component_property='children'),
@@ -165,12 +215,19 @@ app.layout = html.Div(children=[
 )
 def update_graph(value):
     if (value == 'Trump'):
-
         return dcc.Graph(figure=fig2), dcc.Graph(figure=fig5), dcc.Graph(figure=fig7)
     else:
         return dcc.Graph(figure=fig), dcc.Graph(figure=fig4), dcc.Graph(figure=fig6)
 
+###Tab2_content when clicked Tab1###
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+###Multiple Inputs, estimators and samples, contamination [they have to be sliders and drop-down]
+#tab2_content = html.Div([
 
+
+#])
 
 
 if __name__ == '__main__':
